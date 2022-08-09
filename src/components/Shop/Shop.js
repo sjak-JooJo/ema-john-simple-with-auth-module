c60b4d15fbe8a94b677397ff70aa8e41996704dd
JooJo <sjak0101@gmail.com>
2022-08-09 18:46:59 +0100

    added some new function

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,6 +4,7 @@ import './Shop.css';
 
 const Shop = () => {
     const [porducts, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(() => {
         fetch('products.json')
@@ -14,6 +15,9 @@ const Shop = () => {
 
     const handleAddToCart = (product) =>{
         console.log(product);
+        //cart.push(product);
+        const newCart = [...cart, product];
+        setCart(newCart);
    }
 
 
@@ -30,6 +34,7 @@ const Shop = () => {
            </div>
            <div className="cart-container">
             <h4>Order Summary</h4>
+            <p>Selected Items:{cart.length}</p>
            </div>
         </div>
     );
