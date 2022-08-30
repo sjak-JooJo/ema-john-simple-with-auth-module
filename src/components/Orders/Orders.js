15d59a3909e20957d6e2c9324e0e349c3035a3b1
JooJo <sjak0101@gmail.com>
2022-08-30 20:03:18 +0100

    updated order review

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -8,14 +8,20 @@ import ReviewItem from '../ReviewItem/ReviewItem';
 const Orders = () => {
     const [products, setProducts] = useProducts();
     const [cart, setCart] = useCart(products);
+
+    const handleRemoveProduct = product => {
+        const rest = cart.filter(pd => pd.id !== product.id);
+        setCart(rest);
+    }
     return (
         <div>
            <div className="shop-container">
-                <div className="products-container">
+                <div className="review-items-container">
                     {
                         cart.map(product => <ReviewItem
                         key={product.id}
                         product={product}
+                        handleRemoveProduct = {handleRemoveProduct}
                         ></ReviewItem>)
                     }
                 </div>
